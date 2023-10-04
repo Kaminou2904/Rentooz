44fb2dfd3de92e3ccbe3894b34aca5c12a09d69b
vikas kanoo <vikaskanoozzz@gmail.com>
2023-10-04 12:03:46 +0530

    lots of things have changed

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -253,7 +253,7 @@ function Home() {
 
             <div className="callToActionDiv">
                 <p className="grayText text-gray mb-0 mt-3 px-5 lh-sm bricolage-bold text-capitalize text-center">Elevent Your Pune Events with premium event material for rent.</p>
-                <p className="grayText text-brand-blue px-4 mb-0 lh-sm bricolage-extrabold text-capitalize text-center">explore our wide range of products from portable ACs to mist fans, air cooler to electric heater.</p>
+                <p className="grayText text-brand-blue px-4 mb-0 lh-sm bricolage-extrabold text-capitalize text-center">explore our wide range of products from portable ACs to mist fans, air coolers to electric heaters.</p>
                 <div className="container text-center">
                     <button className='callBtn btn border-brand-skin mt-3 text-brand-blue bricolage-bold px-4' onClick={() => window.open('tel:+917666911159', '_self')}><i className="fas fa-phone-alt text-brand-blue me-3"></i>CALL NOW</button>
                 </div>
@@ -266,7 +266,7 @@ function Home() {
                     material rentals that add convenience and charm
                     to your Pune occasions. With a focus on providing
                     top-notch solutions, we offer an extensive selection
-                    of items, including portable ACs, pedeatal fans, mist fans, air cooler and electric heater for rent, mist
+                    of items, including portable ACs, pedeatal fans, mist fans, air coolers and electric heaters for rent, mist
                     fans on rent, and more. Our
                     dedication is to ensure your Pune events are a
                     seamless and comfortable experience</p>
@@ -278,7 +278,7 @@ function Home() {
                     <div className="serviceCardImg mt-3">
                         <img src="./images/5Stand AC.png" className='img-fluid p-2' alt="service icon" />
                     </div>
-                    <p className="mb-0 bricolage-bold text-brand-blue">Portable AC<span className='text-brand-skin bricolage-bold'>s on Rent</span></p>
+                    <p className="mb-0 bricolage-bold text-brand-blue">Portable ACs on Rent</p>
                     <p className="serviceTxt text-gray mb-0 mt-1">Keep your Pune guests cool and comfortable with
                         our stand ACs, designed to deliver efficient
                         cooling for indoor and outdoor events.</p>
