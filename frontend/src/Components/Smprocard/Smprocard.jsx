9e6d82a44b5c09612d027a6216406a14d3fe7899
vikas kanoo <vikaskanoozzz@gmail.com>
2025-04-30 13:35:56 +0530

    small problem solved

--- frontend/src/Components/Smprocard/Smprocard.jsx
+++ frontend/src/Components/Smprocard/Smprocard.jsx
@@ -9,10 +9,16 @@ function Smprocard(props) {
   return (
         <div className='mainsmProcard border-brand-skin rounded-brand'>
             <div className={`smproductcardImg p-3 py-2 ${props.bg}`}>
-                <Link to={`/${props.category.replace(/ /g, '-')}/${props.name.replace(/ /g, '-')}-on-rent-in-pune`} className='nav-link'><img src={props.img} className='img-fluid' alt="handBag" /></Link>
+                <Link to={
+                  path.includes('/buy')? `/buy/${props.category.replace(/ /g, '-')}/${props.name.replace(/ /g, '-')}-in-pune`
+                    : `/${props.category.replace(/ /g, '-')}/${props.name.replace(/ /g, '-')}-on-rent-in-pune`
+                } className='nav-link'><img src={props.img} className='img-fluid' alt="handBag" /></Link>
             </div>
             <div className="mainsmProductcardData p-2">
-                <Link to={`/${props.category.replace(/ /g, '-')}/${props.name.replace(/ /g, '-')}-on-rent-in-pune`} className='nav-link'><p className="mainsmProductcardName bricolage-bold text-capitalize mb-0 text-brand-blue">{props.name.slice(0, 7)}...</p></Link>
+                <Link to={
+                  path.includes('/buy')? `/buy/${props.category.replace(/ /g, '-')}/${props.name.replace(/ /g, '-')}-in-pune`
+                    : `/${props.category.replace(/ /g, '-')}/${props.name.replace(/ /g, '-')}-on-rent-in-pune`
+                } className='nav-link'><p className="mainsmProductcardName bricolage-bold text-capitalize mb-0 text-brand-blue">{props.name.slice(0, 7)}...</p></Link>
                 <p className="mainsmProductcardCategory mb-0 text-muted text-uppercase">{props.category}</p>
                 <div className="mainsmProductcardPrice text-brand-purp lh-sm bricolage-bold">₹{props.rentprice} <span className='text-muted bricolage-semibold'>{path.includes('/buy')? '': '/Day'}</span></div>
             </div>
